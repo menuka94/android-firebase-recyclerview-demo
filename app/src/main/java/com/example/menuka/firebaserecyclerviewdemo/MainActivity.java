1a718d56b93be5ee57d06ea5a73c437933077c0a
Menuka <menuka.14@cse.mrt.ac.lk>
2017-02-17 22:39:21 +0530

    End of ViewPage Video 2

--- app/src/main/java/com/example/menuka/firebaserecyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/menuka/firebaserecyclerviewdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -18,6 +19,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import com.example.menuka.firebaserecyclerviewdemo.utils.Constants;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -92,6 +98,12 @@ public class MainActivity extends AppCompatActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private RecyclerView mRecyclerView;
+        private FirebaseRecyclerAdapter<String, ViewPagerViewHolder> mAdapter;
+        private DatabaseReference mDatabaseReference;
+
+
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -99,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
+            mDatabaseReference = FirebaseDatabase.getInstance().getReference(Constants.POSTS);
         }
 
         /**
@@ -117,9 +130,42 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            // This is where we're going to inflate the RecyclerViews with firebase using the
+            // ARG_SECTION_NUMBER
+            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+            setupAdapter();
+
+
             return rootView;
+        }
+
+        private void setupAdapter() {
+            DatabaseReference tempDatabaseRef = mDatabaseReference.child(Constants.PAGE + "_" + ARG_SECTION_NUMBER);
+            mAdapter = new FirebaseRecyclerAdapter<String, ViewPagerViewHolder>(
+                    String.class,
+                    R.layout.item_card,
+                    ViewPagerViewHolder.class,
+                    tempDatabaseRef
+            ) {
+                @Override
+                protected void populateViewHolder(ViewPagerViewHolder viewHolder, String model, int position) {
+                    viewHolder.setPositionTextView(ARG_SECTION_NUMBER, position);
+                }
+            };
+        }
+
+        public static class ViewPagerViewHolder extends RecyclerView.ViewHolder{
+            private TextView positionTextView;
+
+            public ViewPagerViewHolder(View itemView) {
+                super(itemView);
+                positionTextView = (TextView) itemView.findViewById(R.id.position_text_view);
+            }
+
+            public void setPositionTextView(String page, int n){
+                positionTextView.setText("PAGE: " + page + ", " + String.valueOf(n));
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/menuka/firebaserecyclerviewdemo/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.example.menuka.firebaserecyclerviewdemo.utils;
+
+/**
+ * Created by menuka on 2/17/17.
+ */
+
+public class Constants {
+    public static final String POSTS = "posts";
+    public static final String PAGE = "page";
+}
